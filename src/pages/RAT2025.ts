4611f284d92272529399f0e509b8002e74a968e8
Primayuda <aprita.primayuda@gmail.com>
2025-07-28 10:18:36 +0700

    Using serverless instead of public redirect

---
+++ src/pages/RAT2025.ts
@@ -0,0 +1,6 @@
+// src/pages/RAT2025.ts
+import type { APIRoute } from 'astro';
+
+export const GET: APIRoute = ({ redirect }) => {
+  return redirect('https://zfrmz.com/RgqnnOBNGqeC64jvCSY', 302);
+};
