113b0a883412d0d299332ac6f3a255707580896c
Primayuda <aprita.primayuda@gmail.com>
2025-07-28 11:00:50 +0700

    Using actual URL for Zoho form

--- src/pages/RAT2025.ts
+++ src/pages/RAT2025.ts
@@ -2,5 +2,5 @@
 import type { APIRoute } from 'astro';
 
 export const GET: APIRoute = ({ redirect }) => {
-  return redirect('https://zfrmz.com/RgqnnOBNGqeC64jvCSY', 302);
+  return redirect('https://forms.zohopublic.com/virtualoffice11908/form/RATKDL2025/formperma/ONiESgs6J0kxSJSMYx7mus__Zwkw2TU1m2J5VdxwKZA', 302);
 };
